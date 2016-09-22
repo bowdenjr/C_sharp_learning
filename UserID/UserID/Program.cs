bcdfff1e068833c52334c3e30821bc01de4a0327
Jonathan Bowden <bowden.jr@gmail.com>
2016-09-22 14:52:47 +0100

    Online exercises

---
+++ UserID/UserID/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ID = "jbowden";
+            string password = "hello";
+            int failcounter = 0;
+
+            while(failcounter < 3)
+            {
+                Console.WriteLine("Please enter User ID");
+                string IDentered = Console.ReadLine();
+                Console.WriteLine("Please enter password");
+                string passwordEntered = Console.ReadLine();
+
+                if (IDentered == ID & passwordEntered == password)
+                {
+                    Console.WriteLine("Access granted...");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    failcounter++;
+                    if(failcounter < 3)
+                    { 
+                        Console.WriteLine("Error, please try again, failed attmpts = " + failcounter + " of 3");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exceeded logon attempts, access locked");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+
+
+            }
+
+
+            
+
+        }
+    }
+}
---
+++ w3exercises/w3exercises/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w3exercises
+{
+
+    //http://www.w3resource.com/csharp-exercises/data-types/index.php
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a desired number...");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter a desired length...");
+            int l = Convert.ToInt32(Console.ReadLine());
+
+            while (l >= 1)
+            {
+
+                while (a.Length < l)
+                {
+                    a = a + a;
+                }
+
+                Console.WriteLine(a.Substring(a.Length - l));
+                l--;
+              
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
