a700fb3c07e4abb9474031180640bcaa7935e484
Jonathan Bowden <bowden.jr@gmail.com>
2017-03-28 16:48:18 +0100

    Intitial Cyrpto 2

---
+++ Cryptopals1/Cryptopals1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptopals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // This should be used as the main prcoedure and call the required sub-procedures as coded in the separate programs
+
+
+        }
+    }
+}
